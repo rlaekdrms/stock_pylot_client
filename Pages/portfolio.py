ff155d680c307e9ef706dc108d0c7457e3e5984d
rlaekdrms <danielyoonho@naver.com>
2025-05-03 22:17:59 +0900

    포트폴리오 원형 도넛 그리기

--- Pages/portfolio.py
+++ Pages/portfolio.py
@@ -2,6 +2,8 @@ import streamlit as st
 import os
 import json
 import time
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
 
 ## 페이지 구성
@@ -42,7 +44,7 @@ st.header("📊 현재 포트폴리오")
 if portfolio:
     st.table(portfolio)
     # TODO: 과제, 여기 데이터를 보기 예쁘게 바꾸기(차트 등 활용)
-
+    
 else:
     st.write("보유 종목이 없습니다")
 
@@ -72,3 +74,49 @@ if st.sidebar.button("추가하기"):
         save_portfolio(portfolio)
     
     st.rerun()
+
+def aggregate_portfolio(portfolio_list):
+    aggregated = defaultdict(lambda:{"total_quantity":0,"total_cost":0})
+
+    for item in portfolio_list:
+        code = item["code"]
+        qty = item["quantity"]
+        avg_price = item["avg_price"]
+        aggregated[code]["total_quantity"] += qty
+        aggregated[code]["total_cost"] += qty * avg_price
+
+    result = []     
+    for code, data in aggregated.items():
+        total_qty = data["total_quantity"]
+        total_cost = data["total_cost"]
+        result.append(
+            {
+                "code":code,
+                "avg_price":round(total_cost/total_qty,2),
+                "quantity":total_qty,
+            }
+        )
+    return result
+
+st.subheader("📊 포트폴리오 요약")
+aggregated_portfolio = aggregate_portfolio(portfolio_list)
+
+if aggregated_portfolio:
+    col1, col2 = st.columns([2,3])
+    with col1:
+        labels = [item["code"]for item in aggregated_portfolio]
+        sizes = [item["quantity"]for item in aggregated_portfolio]
+
+        fig, ax = plt.subplots()
+        
+        wedges, text, autotexts = ax.pie(
+            sizes,
+            labels=labels,
+            autopct="%1.1f%%",
+            startangle=90,
+            wedgeprops=dict(width=0.4),
+        )
+        
+        ax.axis("equal")
+        st.pyplot(fig)
+
